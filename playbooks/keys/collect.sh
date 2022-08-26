c05342011a158ada3347440642e20980a136c959
Grayton Ward <graytonio.ward@gmail.com>
2022-08-26 02:00:20 -0400

    Added push keys playbook

---
+++ playbooks/keys/collect.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_PATH="${BASH_SOURCE}"
+while [ -L "${SCRIPT_PATH}" ]; do
+  SCRIPT_DIR="$(cd -P "$(dirname "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+  SCRIPT_PATH="$(readlink "${SCRIPT_PATH}")"
+  [[ ${SCRIPT_PATH} != /* ]] && SCRIPT_PATH="${SCRIPT_DIR}/${SCRIPT_PATH}"
+done
+SCRIPT_PATH="$(readlink -f "${SCRIPT_PATH}")"
+SCRIPT_DIR="$(cd -P "$(dirname -- "${SCRIPT_PATH}")" >/dev/null 2>&1 && pwd)"
+
+KEYFILE="$SCRIPT_DIR/../allkeys.key"
+
+rm -f $KEYFILE
+touch $KEYFILE
+
+for dir in $SCRIPT_DIR/*.key
+do
+    cat $dir >> $KEYFILE
+done
